76811c953b4e9e874c18d8e5d892ab31cbcfeeb0
ikson12 <iksonraj1207@gmail.com>
2025-08-13 10:01:24 +0530

    tasks

--- DAY 7/script.js
+++ DAY 7/script.js
@@ -442,9 +442,8 @@ console.log(`Others : ${others}`);
 	Flag 2 is LUKE WARM
 
 
-	 */
+	 
 
-	// Q15: Heat status using switch statement
 
 let flag = parseInt(prompt("Enter Heat code (1-3):"));
 let status;
@@ -466,3 +465,52 @@ switch (flag) {
 
 console.log(`Flag ${flag} is ${status}`);
 
+*/
+/** Q16 
+	 *  This program uses a switch statement to examine the value of a 
+	 char-type variable called colour and prints one of the following
+	 messages, depending on the character assigned:
+		RED if either r or R is assigned to colour.
+		GREEN if either g or G is assigned to colour.
+		BLUE if either b or B is assigned to colour.
+		BLACK if any other character is assigned to colour.
+		
+		Problem Flow:
+		Prompt the user to enter a single character for the color code.
+		Use a switch statement to check the character and assign the corresponding color.
+		Print the entered color code and the corresponding color name.
+
+		Sample Input / Output:
+		Input:
+		Enter Color code character: g
+		Output:
+		Color Code g is GREEN
+
+
+
+	
+
+let colour = prompt("Enter Color code character:"); 
+let colorName;
+
+switch (colour) {
+    case 'r':
+    case 'R':
+        colorName = "RED";
+        break;
+    case 'g':
+    case 'G':
+        colorName = "GREEN";
+        break;
+    case 'b':
+    case 'B':
+        colorName = "BLUE";
+        break;
+    default:
+        colorName = "BLACK";
+        break;
+}
+
+console.log(`Color Code ${colour} is ${colorName}`);
+
+	 */
